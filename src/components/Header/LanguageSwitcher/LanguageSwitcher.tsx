8cfc088aab8277e138fb591d367b22fb6b653c41
Evgeniya <e.polozova@corp.vk.com>
2021-06-13 22:33:40 +0300

    feat(LanguageSwitcher): add interactivity

--- src/components/Header/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/Header/LanguageSwitcher/LanguageSwitcher.tsx
@@ -2,18 +2,54 @@ import React from 'react';
 import './LanguageSwitcher.css';
 
 interface LanguageButtonProps {
-  children: React.ReactNode;
+  isActive: boolean;
+  children?: any;
+  onClick?: () => void;
 }
 
-const LanguageButton = (props: LanguageButtonProps) => (
-  <button {...props} className="lang button typography--18px" type="button" />
-);
+const LanguageButton: React.FC<LanguageButtonProps> = ({
+  isActive,
+  children,
+  ...props
+}: LanguageButtonProps) => {
+  const activeClassName = isActive ? '' : 'button--link';
+
+  return (
+    <React.Fragment>
+      <button
+        type="button"
+        className={`lang button ${activeClassName} typography--18px`}
+        {...props}
+        disabled={isActive}
+        aria-pressed={isActive}
+      >
+        {children}
+      </button>
+    </React.Fragment>
+  );
+};
 
 export const LanguageSwitcher = () => {
+  const [activeLanguage, switchActiveLanguage] = React.useState<string>('ru');
+
   return (
     <div className="lang__container">
-      <LanguageButton>RU</LanguageButton>
-      <LanguageButton>EN</LanguageButton>
+      <LanguageButton
+        isActive={activeLanguage === 'ru'}
+        onClick={() => {
+          switchActiveLanguage('ru');
+        }}
+      >
+        <span aria-label="Переключиться на русский язык">Рус</span>
+      </LanguageButton>
+      <LanguageButton
+        isActive={activeLanguage === 'en'}
+        onClick={() => {
+          switchActiveLanguage('en');
+        }}
+      >
+        <span aria-label="Switch to English">Eng</span>
+      </LanguageButton>
     </div>
   );
 };
