c5042a08eff780ee919adb0ea8da3d72ffa055ff
Evgeniya <e.polozova@corp.vk.com>
2021-06-13 20:25:47 +0300

    feat(stream): update iframe styles

--- src/components/ArticleStreams/ArticleStreams.tsx
+++ src/components/ArticleStreams/ArticleStreams.tsx
@@ -8,16 +8,17 @@ export const ArticleStreams = () => (
       общечеловеческому». Лекцию прочитает заместитель директора ГМИИ им. А.С.
       Пушкина по региональному развитию Екатерина Кочеткова.
     </p>
-    <iframe
-      className="stream"
-      width="560"
-      height="315"
-      src="https://www.youtube-nocookie.com/embed/5qRR-5H1XgU"
-      aria-labelledby="stream-desc"
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></iframe>
+    <div className="stream" role="presentation">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/5qRR-5H1XgU"
+        aria-labelledby="stream-desc"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
   </React.Fragment>
 );
