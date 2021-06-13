ccfe050506577d194c3d6fae95085274ddb89cb0
Evgeniya <e.polozova@corp.vk.com>
2021-06-13 17:33:03 +0300

    feat(LanguageSwitcher): add basic styles & markup

--- src/components/Header/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/Header/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './LanguageSwitcher.css';
 
+interface LanguageButtonProps {
+  children: React.ReactNode;
+}
+
+const LanguageButton = (props: LanguageButtonProps) => (
+  <button {...props} className="lang button typography--18px" type="button" />
+);
+
 export const LanguageSwitcher = () => {
-  return <div>переключалка языков</div>;
+  return (
+    <div className="lang__container">
+      <LanguageButton>RU</LanguageButton>
+      <LanguageButton>EN</LanguageButton>
+    </div>
+  );
 };
